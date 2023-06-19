c1a3cf9f835c3d485a0c878ea7233fdbea0c3e59
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-06-19 00:28:52 -0400

    Correção de bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import GlobalStyle from './Styles/global';
 import Header from './components/Header';
 import Resume from './components/Resume';
 import Form from './components/Form';
 
 const App = () => {
-    const data = localStorage.getItem("transactions")
+    const data = localStorage.getItem("transactions");
     const [transactionsList, setTransactionsList] = useState(
         data ? JSON.parse(data) : []
     );
@@ -14,9 +14,13 @@ const App = () => {
     const [total, setTotal] = useState(0);
 
     useEffect(() => {
-        const amountExpense = transactionsList.filter((item) => item.expense).map((transaction) => Number(transaction.amount));
+        const amountExpense = transactionsList
+        .filter((item) => item.expense)
+        .map((transaction) => Number(transaction.amount));
 
-        const amountIncome = transactionsList.filter((item) => item.expense).map((transaction) => Number(transaction.amount));
+        const amountIncome = transactionsList
+        .filter((item) => !item.expense)
+        .map((transaction) => Number(transaction.amount));
 
         const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
         const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
@@ -25,10 +29,8 @@ const App = () => {
 
         setIncome(`R$ ${income}`);
         setExpense(`R$ ${expense}`);
-        setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`)
-
+        setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
     }, [transactionsList]);
-
 
     const handleAdd = (transaction) => {
         const newArrayTransactions = [...transactionsList, transaction];
@@ -40,13 +42,16 @@ const App = () => {
 
     return (
         <>
-        <Header/>
-        <Resume income={income} expense={expense} total={total}/>
-        <Form handleAdd={handleAdd}/>
+        <Header />
+        <Resume income={income} expense={expense} total={total} />
+        <Form 
+        handleAdd={handleAdd}
+        transactionsList={transactionsList}
+        setTransactionsList={setTransactionsList}
+    />
         <GlobalStyle />
         </>
     );
 };
 
 export default App;
-
